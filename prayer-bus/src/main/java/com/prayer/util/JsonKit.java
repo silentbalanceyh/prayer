d1408ad14f570f623465440e5f9b2072c0d38f86
Lang <silentbalanceyh@126.com>
2015-06-26 12:35:29 +0800

    去掉配置信息

--- prayer-bus/src/main/java/com/prayer/util/JsonKit.java
+++ prayer-bus/src/main/java/com/prayer/util/JsonKit.java
@@ -1,7 +1,20 @@
 package com.prayer.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.prayer.exception.unchecked.JsonParserException;
+import com.prayer.exception.unchecked.ResourceIOException;
 
 /**
  * 
@@ -12,16 +25,45 @@ public final class JsonKit {
 	// ~ Static Fields =======================================
 	/** **/
 	private static final Logger LOGGER = LoggerFactory.getLogger(JsonKit.class);
+	/** **/
+	private static final ObjectMapper MAPPER = new ObjectMapper();
+
 	// ~ Instance Fields =====================================
 	// ~ Static Block ========================================
 	// ~ Static Methods ======================================
+	/**
+	 * 
+	 * @param filePath
+	 * @return
+	 */
+	public static Map<String, JsonNode> readJson(final String filePath) {
+		Map<String, JsonNode> retMap = null;	// NOPMD
+		try {
+			final InputStream inStream = IOKit.getFile(filePath, JsonKit.class);
+			retMap = MAPPER.readValue(inStream, // NOPMD
+					new TypeReference<Map<String, JsonNode>>() {
+					});
+		} catch (JsonMappingException | JsonParseException ex) {
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("[E] Json parsing error.", ex);
+			}
+			throw new JsonParserException(JsonKit.class, ex.getMessage()); // NOPMD
+		} catch (IOException ex) {
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("[E] Resource I/O error.", ex);
+			}
+			throw new ResourceIOException(JsonKit.class, ex.getMessage()); // NOPMD
+		}
+		return retMap;
+	}
+
 	// ~ Constructors ========================================
 	// ~ Abstract Methods ====================================
 	// ~ Override Methods ====================================
 	// ~ Methods =============================================
 	// ~ Private Methods =====================================
-	
-	private JsonKit(){
+
+	private JsonKit() {
 	}
 	// ~ hashCode,equals,toString ============================
 }
