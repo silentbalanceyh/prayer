3586d8352c09b2430e882c1aa510b6b14d743015
Lang <silentbalanceyh@126.com>
2015-09-03 07:57:37 +0800

    提供Config的业务逻辑层，从Json直接读取OOB的Deploy过程，从数据库读取所有的配置数据

--- prayer-bus/src/main/java/com/prayer/bus/ConfigService.java
+++ prayer-bus/src/main/java/com/prayer/bus/ConfigService.java
@@ -1,9 +1,26 @@
 package com.prayer.bus;
+
+import java.util.concurrent.ConcurrentMap;
+
+import com.prayer.model.bus.ServiceResult;
+import com.prayer.model.bus.VerticleChain;
+
 /**
  * 
  * @author Lang
  *
  */
 public interface ConfigService {
-
+	/**
+	 * 按Group从H2读取Verticle的配置信息
+	 * @param group
+	 * @return
+	 */
+	ServiceResult<VerticleChain> findVerticles(final String group);
+	/**
+	 * 从Json文件导入Verticle配置数据到H2数据库中
+	 * @param jsonPath
+	 * @return
+	 */
+	ServiceResult<ConcurrentMap<String,VerticleChain>> readFromJson(final String jsonPath);
 }
--- prayer-bus/src/main/java/com/prayer/model/bus/VerticleChain.java
+++ prayer-bus/src/main/java/com/prayer/model/bus/VerticleChain.java
@@ -6,12 +6,14 @@ import java.util.List;
 
 import com.prayer.model.h2.vx.VerticleModel;
 
+import jodd.util.StringUtil;
+
 /**
  * 
  * @author Lang
  *
  */
-public final class VerticleChain implements Serializable{
+public final class VerticleChain implements Serializable {
 	// ~ Static Fields =======================================
 	/**
 	 * 
@@ -24,33 +26,40 @@ public final class VerticleChain implements Serializable{
 	private transient final List<VerticleModel> syncList;
 	/** 这个组中的异步发布Verticle的列表 **/
 	private transient final List<VerticleModel> asyncList;
+
 	// ~ Static Block ========================================
 	// ~ Static Methods ======================================
 	// ~ Constructors ========================================
 	/** **/
-	public VerticleChain(final String name){
+	public VerticleChain(final String name) {
 		this.name = name;
 		this.syncList = new ArrayList<>();
 		this.asyncList = new ArrayList<>();
 	}
+
 	// ~ Abstract Methods ====================================
 	// ~ Override Methods ====================================
 	// ~ Methods =============================================
 	/**
-	 * 根据输入的List 
+	 * 根据输入的List
+	 * 
 	 * @param inputList
 	 */
-	public void initVerticles(final List<VerticleModel> inputList){
-		for(final VerticleModel item: inputList){
-			if(item.isDeployAsync()){
-				// 异步Deployment Queue
-				this.asyncList.add(item);
-			}else{
-				// 同步Deployment Queue
-				this.syncList.add(item);
+	public void initVerticles(final List<VerticleModel> inputList) {
+		for (final VerticleModel item : inputList) {
+			// 只可以存放当前Group中的VerticleModel
+			if (StringUtil.equals(item.getGroup(), name)) {
+				if (item.isDeployAsync()) {
+					// 异步Deployment Queue
+					this.asyncList.add(item);
+				} else {
+					// 同步Deployment Queue
+					this.syncList.add(item);
+				}
 			}
 		}
 	}
+
 	// ~ Private Methods =====================================
 	// ~ Get/Set =============================================
 	/**
@@ -59,18 +68,22 @@ public final class VerticleChain implements Serializable{
 	public String getName() {
 		return name;
 	}
+
 	/**
-	 * @param name the name to set
+	 * @param name
+	 *            the name to set
 	 */
 	public void setName(final String name) {
 		this.name = name;
 	}
+
 	/**
 	 * @return the syncList
 	 */
 	public List<VerticleModel> getSyncList() {
 		return syncList;
 	}
+
 	/**
 	 * @return the asyncList
 	 */
