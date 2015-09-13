6163fea67d1bd36dadaaa8787f836ac8b4ab0ef3
Lang <silentbalanceyh@126.com>
2015-09-13 22:30:02 +0800

    添加UniqueValidator验证用户名是否存在

--- prayer-ws/src/main/java/com/prayer/handler/web/FailureHandler.java
+++ prayer-ws/src/main/java/com/prayer/handler/web/FailureHandler.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 
 import com.prayer.constant.Constants;
 import com.prayer.constant.Resources;
-import com.prayer.exception.AbstractException;
 import com.prayer.model.bus.web.RestfulResult;
 import com.prayer.uca.assistant.WebLogger;
 
@@ -51,28 +50,23 @@ public class FailureHandler implements ErrorHandler {
 		info(LOGGER, WebLogger.I_STD_HANDLER, getClass().getName(), Constants.VX_OD_FAILURE);
 		// 1.包装响应信息
 		final HttpServerResponse response = context.response();
-		try {
-			// 2.获取RestfulResult
-			final RestfulResult webRet = (RestfulResult) context.get(Constants.VX_CTX_ERROR);
-			info(LOGGER, WebLogger.I_REST_RESULT, webRet);
-			if(null != webRet.getError()){
-				throw webRet.getError();
-			}
-			// 3.包装Error信息生成统一的Error格式
-			final JsonObject retData = webRet.getResult();
-			// 4.获取响应的信息
-			final String content = retData.encodePrettily();
+		// 2.获取RestfulResult
+		final RestfulResult webRet = (RestfulResult) context.get(Constants.VX_CTX_ERROR);
+		info(LOGGER, WebLogger.I_REST_RESULT, webRet);
+		error(LOGGER, WebLogger.E_COMMON_EXP, webRet.getError());
+		// 3.包装Error信息生成统一的Error格式
+		final JsonObject retData = webRet.getResult();
+		// 4.获取响应的信息
+		final String content = retData.encodePrettily();
 
-			// TODO: 5.后期需要改动，测试因为使用浏览器，暂时使用这种
-			response.putHeader("Context-Type", "application/json;charset=" + Resources.SYS_ENCODING);
-			response.putHeader("Content-Length", String.valueOf(content.getBytes().length));
-			// 6.设置StatusCode和Error
-			response.setStatusCode(retData.getInteger(Constants.STATUS_CODE));
-			response.setStatusMessage(retData.getString(Constants.ERROR));
-			response.write(content, Resources.SYS_ENCODING);
-		} catch (AbstractException ex) {
-			error(LOGGER, WebLogger.E_COMMON_EXP, ex.toString());
-		}
+		// TODO: 5.后期需要改动，测试因为使用浏览器，暂时使用这种
+		response.putHeader("Context-Type", "application/json;charset=" + Resources.SYS_ENCODING);
+		response.putHeader("Content-Length", String.valueOf(content.getBytes().length));
+		// 6.设置StatusCode和Error
+		response.setStatusCode(retData.getInteger(Constants.STATUS_CODE));
+		response.setStatusMessage(retData.getString(Constants.ERROR));
+		response.write(content, Resources.SYS_ENCODING);
+
 		response.end();
 		response.close();
 	}
---
+++ prayer-ws/src/main/java/com/prayer/uca/validator/UniqueValidator.java
@@ -0,0 +1,93 @@
+package com.prayer.uca.validator;
+
+import static com.prayer.util.Instance.singleton;
+
+import java.text.MessageFormat;
+
+import com.prayer.constant.Constants;
+import com.prayer.constant.SqlSegment;
+import com.prayer.constant.Symbol;
+import com.prayer.db.conn.JdbcContext;
+import com.prayer.db.conn.impl.JdbcConnImpl;
+import com.prayer.exception.AbstractWebException;
+import com.prayer.kernel.Value;
+import com.prayer.model.type.DataType;
+import com.prayer.uca.WebValidator;
+import com.prayer.uca.assistant.Extractor;
+import com.prayer.uca.assistant.Interruptor;
+
+import io.vertx.core.json.JsonObject;
+import net.sf.oval.constraint.NotBlank;
+import net.sf.oval.constraint.NotEmpty;
+import net.sf.oval.constraint.NotNull;
+import net.sf.oval.guard.Guarded;
+import net.sf.oval.guard.PostValidateThis;
+
+/**
+ * 
+ * @author Lang
+ *
+ */
+@Guarded
+public class UniqueValidator implements WebValidator {
+	// ~ Static Fields =======================================
+	/** 数据库表名 **/
+	private final static String TABLE = "table";
+	/** 数据库列名 **/
+	private final static String UK_COLUMN = "column";
+
+	// ~ Instance Fields =====================================
+	/** **/
+	@NotNull
+	private transient final JdbcContext context;
+
+	// ~ Static Block ========================================
+	// ~ Static Methods ======================================
+	// ~ Constructors ========================================
+	/** **/
+	@PostValidateThis
+	public UniqueValidator() {
+		this.context = singleton(JdbcConnImpl.class);
+	}
+
+	// ~ Abstract Methods ====================================
+	// ~ Override Methods ====================================
+	/**
+	 * 
+	 */
+	@Override
+	public boolean validate(@NotNull @NotBlank @NotEmpty final String name, @NotNull final Value<?> value,
+			@NotNull final JsonObject config) throws AbstractWebException {
+		// 1.检查Validator提供的配置信息
+		Interruptor.interruptRequired(getClass(), name, config, TABLE);
+		Interruptor.interruptRequired(getClass(), name, config, UK_COLUMN);
+		Interruptor.interruptStringConfig(getClass(), name, config, TABLE);
+		Interruptor.interruptStringConfig(getClass(), name, config, UK_COLUMN);
+		// 2.检查值
+		final String table = Extractor.getString(config, TABLE);
+		final String column = Extractor.getString(config, UK_COLUMN);
+		// 3.数据库访问查询
+		return this.verifyUnique(table, column, value);
+	}
+
+	// ~ Methods =============================================
+	// ~ Private Methods =====================================
+
+	private boolean verifyUnique(final String table, final String column, final Value<?> value) {
+		final StringBuilder sql = new StringBuilder(Constants.BUFFER_SIZE);
+		sql.append(MessageFormat.format(SqlSegment.TB_COUNT, table)).append(Symbol.SPACE).append(SqlSegment.WHERE)
+				.append(Symbol.SPACE).append(column).append(Symbol.SPACE).append(Symbol.EQUAL).append(Symbol.SPACE);
+		if (value.getDataType() == DataType.STRING || value.getDataType() == DataType.DATE
+				|| value.getDataType() == DataType.XML || value.getDataType() == DataType.JSON
+				|| value.getDataType() == DataType.SCRIPT) {
+			sql.append(Symbol.S_QUOTES).append(value.literal()).append(Symbol.S_QUOTES);
+		} else if (value.getDataType() == DataType.INT || value.getDataType() == DataType.LONG
+				|| value.getDataType() == DataType.BOOLEAN || value.getDataType() == DataType.DECIMAL) {
+			sql.append(value.literal());
+		}
+		final Long ret = this.context.count(sql.toString());
+		return ret == Constants.ZERO;
+	}
+	// ~ Get/Set =============================================
+	// ~ hashCode,equals,toString ============================
+}
