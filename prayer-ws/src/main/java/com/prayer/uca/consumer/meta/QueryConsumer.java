9129f3e5e739c80913ee25588064eefcdc532ba4
Lang <silentbalanceyh@126.com>
2015-11-17 21:51:27 +0800

    添加元数据分页信息

---
+++ prayer-ws/src/main/java/com/prayer/uca/consumer/meta/QueryConsumer.java
@@ -0,0 +1,84 @@
+package com.prayer.uca.consumer.meta;
+
+import static com.prayer.assistant.WebLogger.info;
+import static com.prayer.util.Converter.fromStr;
+import static com.prayer.util.Instance.singleton;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.prayer.assistant.Extractor;
+import com.prayer.assistant.WebLogger;
+import com.prayer.bus.std.MetaService;
+import com.prayer.bus.std.impl.MetaSevImpl;
+import com.prayer.constant.Constants;
+import com.prayer.exception.web.MethodNotAllowedException;
+import com.prayer.model.bus.ServiceResult;
+import com.prayer.model.web.Responsor;
+import com.prayer.model.web.StatusCode;
+
+import io.vertx.core.Handler;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.JsonObject;
+import net.sf.oval.constraint.NotNull;
+import net.sf.oval.guard.Guarded;
+import net.sf.oval.guard.PreValidateThis;
+
+/**
+ * 
+ * @author Lang
+ *
+ */
+@Guarded
+public class QueryConsumer implements Handler<Message<Object>> {
+    // ~ Static Fields =======================================
+    /** **/
+    private static final Logger LOGGER = LoggerFactory.getLogger(QueryConsumer.class);
+    // ~ Instance Fields =====================================
+    /** **/
+    @NotNull
+    private transient final MetaService metaSev;
+
+    // ~ Static Block ========================================
+    // ~ Static Methods ======================================
+    // ~ Constructors ========================================
+    /** **/
+    public QueryConsumer() {
+        this.metaSev = singleton(MetaSevImpl.class);
+    }
+
+    // ~ Abstract Methods ====================================
+    // ~ Override Methods ====================================
+    /** **/
+    @Override
+    @PreValidateThis
+    public void handle(@NotNull final Message<Object> event) {
+        info(LOGGER, WebLogger.I_COMMON_INFO, "Consumer -> " + getClass().toString());
+        // 1.从EventBus中接受数据
+        final JsonObject params = (JsonObject) event.body();
+        // 2.获取方法信息
+        final HttpMethod method = fromStr(HttpMethod.class, params.getString(Constants.PARAM.METHOD));
+        // 3.根据不同的方法的Record
+        Responsor responsor = null;
+        switch (method) {
+        // 只有POST方法才能触发这种分页的信息
+        case POST: {
+            final ServiceResult<JsonObject> result = this.metaSev.page(params);
+            responsor = Extractor.responsor(result, StatusCode.OK);
+        }
+            break;
+        default: {
+            responsor = Responsor.failure(StatusCode.METHOD_NOT_ALLOWED,
+                    new MethodNotAllowedException(getClass(), method.toString()));
+        }
+            break;
+        }
+        event.reply(responsor.getResult());
+    }
+    // ~ Methods =============================================
+    // ~ Private Methods =====================================
+    // ~ Get/Set =============================================
+    // ~ hashCode,equals,toString ============================
+
+}
--- prayer-ws/src/test/java/com/prayer/deploy/DebuggerTestCase.java
+++ prayer-ws/src/test/java/com/prayer/deploy/DebuggerTestCase.java
@@ -1,5 +1,10 @@
 package com.prayer.deploy;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.junit.Test;
 
 import com.prayer.bus.std.RecordService;
@@ -14,24 +19,48 @@ public class DebuggerTestCase {
     // ~ Static Fields =======================================
     // ~ Instance Fields =====================================
     private transient RecordService service = new RecordSevImpl();
+
     // ~ Static Block ========================================
     // ~ Static Methods ======================================
     // ~ Constructors ========================================
     // ~ Abstract Methods ====================================
     // ~ Override Methods ====================================
     // ~ Methods =============================================
-    public void testSelect(){
-        final JsonObject params = new JsonObject("{\"identifier\":\"RP$identifier\",\"filters\":[],\"method\":\"GET\",\"data\":{\"name\":\"username\",\"identifier\":\"sec.account\",\"value\":\"lang.yu\"},\"script\":\"js.api.get.util.unique\"}");
+    public void testSelect() {
+        final JsonObject params = new JsonObject(
+                "{\"identifier\":\"RP$identifier\",\"filters\":[],\"method\":\"GET\",\"data\":{\"name\":\"username\",\"identifier\":\"sec.account\",\"value\":\"lang.yu\"},\"script\":\"js.api.get.util.unique\"}");
         ServiceResult<JsonArray> ret = service.find(params);
         System.out.println(ret.getResult().encode());
     }
-    
-    @Test
-    public void testFolder(){
+
+    public void testFolder() {
         System.out.println(IOKit.listFiles("deploy/oob/vertx/route"));
     }
+
+    @Test
+    public void testProcess() throws Exception {
+
+    }
+
     // ~ Private Methods =====================================
     // ~ Get/Set =============================================
     // ~ hashCode,equals,toString ============================
-
+    public static void main(String args[]) throws Exception {
+        Process proc = Runtime.getRuntime().exec("tasklist /FI \"imagename eq chrome.exe\"");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        String line = null;
+        while ((line = bufferedReader.readLine()) != null) {
+            if(line.indexOf("4204") >= 0){
+                System.out.println(line);
+                Pattern pattern = Pattern.compile("(\\d+)(,\\d+)*\\s(K|k)");
+                Matcher matcher = pattern.matcher(line);
+                if(matcher.find()){
+                    Long kb = Long.parseLong(matcher.group().replace(",", "").replace("K", "").trim());
+                    kb = kb / 1024;
+                    System.out.println(kb);
+                    break;
+                }
+            }
+        }
+    }
 }
