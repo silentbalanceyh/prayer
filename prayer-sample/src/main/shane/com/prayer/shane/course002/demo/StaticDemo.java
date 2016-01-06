2854a21027260cc96607a234f075c6fb4333d68f
Lang <silentbalanceyh@126.com>
2016-01-06 23:05:00 +0800

    Course002

---
+++ prayer-sample/src/main/shane/com/prayer/shane/course002/demo/StaticDemo.java
@@ -0,0 +1,18 @@
+package com.prayer.shane.course002.demo;
+
+public class StaticDemo {
+    // ~ Static Fields =======================================
+    // ~ Instance Fields =====================================
+    // ~ Static Block ========================================
+    // ~ Static Methods ======================================
+    // ~ Constructors ========================================
+    // ~ Abstract Methods ====================================
+    // ~ Override Methods ====================================
+    // ~ Methods =============================================
+    // ~ Private Methods =====================================
+    // ~ Get/Set =============================================
+    // ~ hashCode,equals,toString ============================
+    public static void main(String args[]){
+
+    }
+}
