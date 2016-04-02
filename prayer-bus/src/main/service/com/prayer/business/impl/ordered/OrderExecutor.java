58a015bfe54bac58bb4364a31b8d84a178b463e0
Lang <silentbalanceyh@126.com>
2016-04-02 12:40:39 +0800

    执行Order计算

--- prayer-bus/src/main/service/com/prayer/business/impl/ordered/OrderExecutor.java
+++ prayer-bus/src/main/service/com/prayer/business/impl/ordered/OrderExecutor.java
@@ -3,6 +3,8 @@ package com.prayer.business.impl.ordered;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +15,9 @@ import com.prayer.util.io.IOKit;
 
 import io.vertx.core.json.DecodeException;
 import io.vertx.core.json.JsonObject;
+import net.sf.oval.constraint.NotBlank;
+import net.sf.oval.constraint.NotEmpty;
+import net.sf.oval.constraint.NotNull;
 import net.sf.oval.guard.Guarded;
 
 /**
@@ -29,7 +34,8 @@ public class OrderExecutor {
 
     // ~ Instance Fields =====================================
     /** 获取原始的列表 **/
-    private transient List<OrderNode> rawList = new ArrayList<>();
+    private transient ConcurrentMap<String, OrderNode> rawMap = new ConcurrentHashMap<>();
+
     // ~ Static Block ========================================
     // ~ Static Methods ======================================
     // ~ Constructors ========================================
@@ -37,22 +43,35 @@ public class OrderExecutor {
     // ~ Override Methods ====================================
     // ~ Methods =============================================
     /** 执行分析，得出最终的Model的定义顺序 **/
-    public Map<Integer, String> execute(final String folder) throws RecurrenceReferenceException {
-        /** 1.读取所有的文件 **/
-        final List<String> files = IOKit.listFiles(folder);
-        final List<OrderNode> nodes = new ArrayList<>();
+    public Map<Integer, String> execute(@NotNull @NotEmpty @NotBlank final String folder)
+            throws RecurrenceReferenceException {
+        /** 1.读取所有的文件，初始化基础节点 **/
+        final List<OrderNode> nodeList = this.buildNodeList(folder);
         /** 2.初始化所有的基础节点 **/
-        for(final String file: files){
-            final OrderNode node = this.initNode(folder + '/' + file);
-            if(null != node && node.isValid()){
-                nodes.add(node);
-                System.out.println(node);
-            }
-        }
+
         return null;
     }
 
     // ~ Private Methods =====================================
+    
+    private void buildRefs(final List<OrderNode> nodeList){
+        
+    }
+
+    private List<OrderNode> buildNodeList(final String folder) {
+        final List<String> files = IOKit.listFiles(folder);
+        final List<OrderNode> nodes = new ArrayList<>();
+        for (final String file : files) {
+            final OrderNode node = this.initNode(folder + '/' + file);
+            if (null != node && node.isValid()) {
+                nodes.add(node);
+                rawMap.put(node.getTable(), node);
+                System.out.println(node);
+            }
+        }
+        return nodes;
+    }
+
     /**
      * 只执行创建验证，不执行更新验证，并且过滤需要验证数据库表存在的情况
      * 
